fbef775c3bc0290865b7c1e18d5e3780d8d1bd7d
Gavi <User@Yuri>
2015-05-18 19:47:13 -0500

    Merge origin/master
    Conflicts:
    	Hora_Saludable/web/plantillas/frontEndAdministrador.xhtml
    	Hora_Saludable/web/plantillas/frontEndUsuarioconysinSesion.xhtml

--- Hora_Saludable/src/java/com/unicauca/horasaludable/managedbeans/contenidos/horarioController.java
+++ Hora_Saludable/src/java/com/unicauca/horasaludable/managedbeans/contenidos/horarioController.java
@@ -6,20 +6,16 @@
 package com.unicauca.horasaludable.managedbeans.contenidos;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
