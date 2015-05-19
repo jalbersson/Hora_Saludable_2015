203348319ed0c2fd0c655d985f698c3d17db7781
Gavi <User@Yuri>
2015-05-18 19:40:58 -0500

    Merge origin/master
    Conflicts:
    	Hora_Saludable/web/plantillas/frontEndAdministrador.xhtml
    	Hora_Saludable/web/plantillas/frontEndUsuarioconysinSesion.xhtml

--- Hora_Saludable/src/java/com/unicauca/horasaludable/managedbeans/contenidos/ImagesView.java
+++ Hora_Saludable/src/java/com/unicauca/horasaludable/managedbeans/contenidos/ImagesView.java
@@ -5,10 +5,13 @@
  */
 package com.unicauca.horasaludable.managedbeans.contenidos;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
  
 /**
  *
@@ -18,9 +21,20 @@ import javax.faces.bean.ManagedBean;
 public class ImagesView 
 {
     private List<String> images;
+    private List<String> images2;
+    
+    String dir;
+    File f;
      
     @PostConstruct
     public void init() {
+        
+        ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String realPath = (String) servletContext.getRealPath("/"); // Sustituye "/" por el directorio ej: "/upload"
+        dir = realPath + "\\resources\\img\\imagenesNoticias\\";
+        
+        f = new File(dir);
+        
         images = new ArrayList<String>();
         for (int i = 1; i <= 5; i++) {
             images.add("salud" + i + ".jpg");
@@ -29,5 +43,23 @@ public class ImagesView
  
     public List<String> getImages() {
         return images;
+    }
+    
+    public String nomDirectorio()
+    {
+        String result="";
+        
+        if(f.exists())
+        {
+            File[] ficheros = f.listFiles();
+            for(int i=0 ; i<ficheros.length ; i++)
+            {
+                images2.add(ficheros[i].getName());
+            }
+            result = images2.get(images2.size()-1).toString();
+        }
+        
+        
+        return result;
     }
 }
